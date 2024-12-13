8884ebe79bdbb27eed5a1336909fff7a29ba4a88
Ahyun-Song <yoon9113@naver.com>
2024-12-13 12:33:35 +0900

    Add project files.

---
+++ WindowsFormsApp_241213/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_241213
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox_print.Text = "이것은\r\n멀티박스\r\n텍스트박스";
+            // MessageBox.Show("Hello");
+        }
+
+
+    }
+}
